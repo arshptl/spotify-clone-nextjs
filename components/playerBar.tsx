421e5c8e3a1629ce461f7cd2bc54cb9e1d0138f1
harshptl14 <harshptl714@gmail.com>
2022-05-16 10:25:05 +0530

    Initialized project

---
+++ components/playerBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PlayerBar = () => {
+  return (
+    <div>playerBaP</div>
+  )
+}
+
+export default PlayerBar
---
+++ components/sidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>hello from sidebar</div>
+  )
+}
+
+export default Sidebar
